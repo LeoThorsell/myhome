227d9a92a8494ef783a64c00f4c81a89e8879ada
Leo Thorsell <thorsell.spel@gmail.com>
2014-01-04 18:47:42 +0000

    changed tellstick lib and now tellstickHw supports creating new devices.

--- tellstickHw.js
+++ tellstickHw.js
@@ -1,4 +1,5 @@
-var tellstick = require('telldus-core-js'),
+var tellstick = require('telldus'),
+	async = require('async'),
 	redis = require("redis"),
         client = redis.createClient(),
 	client2 = redis.createClient();
@@ -11,20 +12,60 @@ client.on("error", function (err) {
 //	client2.lpush('user:1.tellstick:1', values);
 //	client2.ltrim('user:1.tellstick:1', '0', '99');
 //});
-var createDevice = function(actuator){
-//	var newDevice = tellstick.
-	console.log('create tellstick device is not implemented yet!'); // TODO: implementera
-};
-var populateDevices = function(){
-	var tellstickDevices = tellstick.getDevices();
-	var parametersToRead = ['house', 'unit', 'group', 'command']
-	tellstickDevices.forEach(function(d){
-		d.protocol = tellstick.getProtocol(d.id);
-		d.model = tellstick.getModel(d.id);
-		parametersToRead.forEach(function(p){
-			d[p] = tellstick.getDeviceParameter(d.id, p, '');
+var createDevice = function(actuator, callback){
+	console.log(actuator);
+	var newDevice = {}; 
+	var validInputs = ['unit', 'house', 'code']; //Todo: add other inputs
+	for(key in actuator)
+		newDevice[key] = actuator[key];
+	tellstick.addDevice(function(err, id){
+		console.log('new id:' + id);
+		newDevice.id = id;
+		var tasks = [];
+		console.log(newDevice);		
+		tasks.push(function(cb){tellstick.setProtocol(newDevice.id, newDevice.protocol, cb);});
+		tasks.push(function(cb){tellstick.setModel(newDevice.id, newDevice.model, cb);});
+		for(key in newDevice){
+			if(validInputs.indexOf(key)<0)
+				continue;
+			if(newDevice[key] == undefined || newDevice[key].length == 0 )
+				continue;
+			(function(k){
+				tasks.push(function(cb){tellstick.setDeviceParameter(newDevice.id, k, newDevice[k], cb)});
+			})(key);
+		}
+		async.parallel(tasks, function(err, results){
+			devices.push(newDevice);
+			console.log('new device added: id='+newDevice.id);
+			callback(newDevice);
 		});
-		devices.push(d);
+	});
+};
+var populateDevices = function(callback){
+	tellstick.getDevices(function(err, tellstickDevices){
+		var parametersToRead = ['house', 'unit', 'group', 'command'];
+		var deviceTasks = [];
+		tellstickDevices.forEach(function(d){
+			tasks = [];
+			tasks.push(function(cb){tellstick.getProtocol(d.id, cb)});
+			tasks.push(function(cb){tellstick.getModel(d.id, cb)});
+			parametersToRead.forEach(function(p){
+				tasks.push(function(cb){tellstick.getDeviceParameter(d.id, p, '', cb)});
+			});
+			deviceTasks.push(function(cb){
+				async.parallel(tasks, function(err, results){
+					d.protocol = results[0][0];
+					d.model = results[1][0];
+					for(var i=0;i<parametersToRead.length;i++)
+						d[parametersToRead[i]] = results[2+i][0];
+					devices.push(d);
+					cb();
+				});
+			});
+		});
+		async.series(deviceTasks, function(){
+			callback();
+		});
 	});
 };
 var performCommand = function(device, command){
@@ -34,24 +75,31 @@ var performCommand = function(device, command){
 	}
 	console.log(command + ' ' + device.id);	
 	if(command == 'turnoff'){
-		if(tellstick.turnOff(device.id)!=0)
+		tellstick.turnOff(device.id, function(err, val){
+			if(err==null)
+				return;
 			console.log('error turning off device');
+		});
 	}
-	if(command == 'turnon')
-		if(tellstick.turnOn(device.id)!=0)
+	if(command == 'turnon'){
+		tellstick.turnOn(device.id, function(err, val){
+			if(err == null)
+				return;
 			console.log('error turning on device');
+		});
+	}
 };
-var deviceKeysToCompare = ['protocol', 'house', 'unit'];
+var deviceKeysToCompare = ['protocol', 'house', 'unit', 'model'];
 var doAction = function(){
 	client.blpop('user:1.tellstickActuator:1.raw', 0, function(err, result){
 		var actuatorString = result[1];
-		var deviceToOperate = -1;
+		var deviceToOperate = null;
 		console.log('actuator hw, command');
 		var keyValues = actuatorString.split(';');
 		var actuator = {};
 		keyValues.forEach(function(keyValue){
 			var splitted = keyValue.split(':');
-			if(splitted.length != 2)
+			if(splitted.length < 2)
 				return;
 			actuator[splitted[0]]=splitted[1];
 		});
@@ -73,11 +121,17 @@ var doAction = function(){
 			deviceToOperate = device;
 		});
 		if(deviceToOperate == null){
-			deviceToOperate = createDevice(actuator);
+			createDevice(actuator, function(device){
+				performCommand(device, actuator.method);
+			});
+			doAction();
+			return;
 		};
 		performCommand(deviceToOperate, actuator.method);
 		doAction();	
 	});
 };
-doAction();
-populateDevices();
+populateDevices(function(){
+	console.log(devices);
+	doAction();
+});
